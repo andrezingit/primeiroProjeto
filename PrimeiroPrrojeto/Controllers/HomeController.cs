faae2bde6064f243bc5aa54503be00c5ce076051
andre.luiz <andre.npd@uniselva.org.br>
2023-08-24 08:29:28 -0400

    commit teste

---
+++ PrimeiroPrrojeto/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiroPrrojeto.Models;
+using System.Diagnostics;
+
+namespace PrimeiroPrrojeto.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }*/
+
+        public ViewResult Index(int? id)
+        {
+            return View(id);
+        }
+
+        /*public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+    }
+}
---
+++ PrimeiroPrrojeto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrimeiroPrrojeto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
